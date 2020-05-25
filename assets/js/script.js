2e72045fc2fac898745abf0ce527cbd11055f6a1
Amalia Hay <amaliawhay@gmail.com>
2020-05-24 19:59:36 -0700

    changed variables, added collection variable, restructured concatonization, random characters still not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,151 +1,75 @@
+var lower = "abcdefghijklmnopqrstuvwxyz";
+
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+
+var special = "!@#$%^&*()-_+={[}]?<>";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//These are the variables that hold the options the user can input
-var charOptions = {
-  small: [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ],
-
-  big: [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ],
-  numbers: [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ],
-  special: [
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_",
-    "-",
-    ".",
-    ",",
-    "?",
-  ],
-};
-
-var userPassword = prompt(
-  "How many characters do you want your password to have?"
-);
-
-if (userPassword >= 8 && userPassword <= 128) {
-  var smallTrue = confirm(
-    "Do you want your password to have lowecase letters?"
-  );
-  var bigTrue = confirm(
-    "Do you want your password to have uppercase letters?"
-  );
-  var numbersTrue = confirm(
-    "Do you want your password to have numbers?"
-  );
-  var specialTrue = confirm(
-    "Do you want your password to have special characters?"
-  );
-}
 
 // Write password to the #password input
 function writePassword() {
-  for (i = 0; i < userPassword; i++) {
-    if (smallTrue === true) {
-      var password =
-        charOptions.small[
-          Math.floor(
-            Math.random() * charOptions.small.length
-          )
+  var passwordLength = prompt(
+    "How many characters do you want your password to have?"
+  );
+  passwordLength = parseInt(passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert(
+      "Your password will be " +
+        passwordLength +
+        " characters long."
+    );
+  } else {
+    return alert(
+      "Invalid response. Please enter a number."
+    );
+  }
+  var userLower = confirm(
+    "Do you want your password to have lowercase letters?"
+  );
+  var userUpper = confirm(
+    "Do you want your password to have uppercase letters?"
+  );
+  var userNum = confirm(
+    "Do you want your password to have numbers?"
+  );
+  var userSpecial = confirm(
+    "Do you want your password to have special characters?"
+  );
+  var collection = [];
+  if (userLower === true) {
+    collection = collection.concat(lower);
+    console.log(collection);
+  }
+  if (userUpper === true) {
+    collection = collection.concat(upper);
+    console.log(collection);
+  }
+  if (userNum === true) {
+    collection = collection.concat(num);
+    console.log(collection);
+  }
+  if (userSpecial === true) {
+    collection = collection.concat(special);
+    console.log(collection);
+  }
+  // you can create a function named generatePassword that creates the password
+  function generatePassword(passwordLength, collection) {
+    for (var i = 0; i < passwordLength; i++) {
+      var passwordText =
+        collection[
+          Math.floor(Math.random() * collection.length)
         ];
-    } else if (bigTrue === true) {
-      var password =
-        charOptions.big[
-          Math.floor(Math.random() * charOptions.big.length)
-        ];
-    } else if (numbersTrue === true) {
-      var password =
-        charOptions.numbers[
-          Math.floor(
-            Math.random() * charOptions.numbers.length
-          )
-        ];
-    } else if (specialTrue === true) {
-      var password =
-        charOptions.special[
-          Math.floor(
-            Math.random() * charOptions.special.length
-          )
-        ];
+      passwordText += collection.charAt(passwordText);
+      return passwordText;
     }
   }
-
-  // you can create a function named generatePassword that creates the password
-  var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
-console.log(writePassword);
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
